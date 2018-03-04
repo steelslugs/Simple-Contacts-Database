74b4c2d38d038f1711bde4d705e96757d78a0d47
steelslugs <34694950+steelslugs@users.noreply.github.com>
2018-03-04 16:59:33 +0000

    Update Contact_controller.php

--- controllers/Contact_controller.php
+++ controllers/Contact_controller.php
@@ -7,70 +7,108 @@ class Contact_controller extends CI_Controller {
 		$this->load->model('contact_model');
 		$this->load->helper('url_helper');
 	}
-	
-	public function index(){
-		
-		$data['title'] = 'Contacts Database:';
-		$this->load->view('templates/header', $data);
-		$this->load->view('contactsdb/index');
-		$this->load->view('templates/footer');
-	}
-	
-	public function getcontact($slug = NULL){
-		
-		//echo '>>>>>>edit>>>>>'.var_dump($slug);
-		//exit;
-		$this->load->helper('form');
-		$this->load->library('form_validation');
-
-		$data['title'] = 'Get contact';
-		//$this->form_validation->set_rules('contact_id', 'contact_id', 'required');
-
-		if ($this->form_validation->run() === FALSE)
-		{
-			$this->load->view('templates/header', $data);
-			$this->load->view('contactsdb/index');
-			$this->load->view('contactsdb/getcontact', $data);
-			$this->load->view('templates/footer');
-
+	public function viewit($page='home'){
+		if ( ! file_exists(APPPATH.'views/contactsdb/'.$page.'.php'))
+        {
+			echo "Whoops, we don't have a page for that!";
+			
+			show_404();
 		}
-		else
-		{
-			$this->load->view('contactdb/xxxcontact', $data);
+		//$data['title'] = ucfirst($page); // Capitalize the first letter
+		
+		//display heading based on page loaded.
+		switch ($page) {
+			case "getallcontact":
+				$data['title'] = "All Contacts :";
+				break;
+			case "searchcontact":
+				$data['title'] = "Search :";
+				break;
+			case "showcontact":
+				$data['title'] = "Available contacts:";
+				break;
+			case "createcontact":
+				$data['title'] = "Enter contact details:";
+				break;
+			default:
+				$data['title'] = "default header:";
 		}
-	}
-	
-	public function getallcontact(){
+		if ($page==='getallcontact'){
+			$this->load->helper('form');
+			$this->load->library('form_validation');
+			$data['contact'] = $this->contact_model->get_contact();
+		}
+		if ($page==='showcontact'||$page==='editcontact'||$page==='getcontactbyfield'){
+			$this->load->helper('form');
+			$this->load->library('form_validation');
+			$slug=$this->input->post();
+			$data['contact'] = $this->contact_model->get_contact($slug);
+		}
 		
-		$this->load->helper('form');
-		$this->load->library('form_validation');
-		$data['contact'] = $this->contact_model->get_contact();
-		$data['title'] = 'Contacts:';
-
-		$this->load->view('templates/header', $data);
+		
+		if ($page==='updatecontact'){
+			$this->load->helper('form');
+			$this->load->library('form_validation');
+			
+			$this->form_validation->set_rules('salutation', 'salutation', 'required');
+			$this->form_validation->set_rules('first_name', 'first_name', 'required');
+			$this->form_validation->set_rules('last_name', 'last_name', 'required');
+			$this->form_validation->set_rules('DOB', 'Date Of Birth', 'required');
+			$this->form_validation->set_rules('address', 'Address', 'required');
+			$this->form_validation->set_rules('city', 'city', 'required');
+			$this->form_validation->set_rules('postcode', 'postcode', 'required');
+			$this->form_validation->set_rules('tel', 'Telephone', 'required');
+			if ($this->form_validation->run() === FALSE){
+				 
+				$slug=$this->input->post();
+				//echo '>>>>>>>>>>'.var_dump($slug);
+				//exit;
+				$data['contact'] = $this->contact_model->get_contact($slug);
+				// look. edit page is reloaded with the details again.
+				// and a message is displayed to show what is needed by the php validation.
+				$page='editcontact';
+				
+			}
+			else{
+				$slug=$this->input->post();
+				$data['contact'] = $this->contact_model->update_contact($slug);
+				//echo var_dump($page);exit;
+				if($data['contact']>0){
+					$page='success';
+				}
+				else{
+					$page='fail';
+				}
+			}	
+		}
+	
+		
+       
+        $this->load->view('templates/header', $data);
 		$this->load->view('contactsdb/index');
-		$this->load->view('contactsdb/getallcontact', $data);
-		$this->load->view('templates/footer');
+        $this->load->view('contactsdb/'.$page, $data);
+        $this->load->view('templates/footer', $data);
+
 	}
-	
-	
-	public function createcontact(){
+
+		public function createcontact(){
 		
 		$this->load->helper('form');
+		$this->load->helper(array('form', 'url'));
 		$this->load->library('form_validation');
-		// $this->form_validation->set_rules('salutation', 'salutation', 'required');
-		// $this->form_validation->set_rules('first_name', 'first_name', 'required');
-		// $this->form_validation->set_rules('last_name', 'last_name', 'required');
-		// $this->form_validation->set_rules('DOB', 'Title', 'DOB');
-		// $this->form_validation->set_rules('address', 'address', 'required');
-		// $this->form_validation->set_rules('city', 'city', 'required');
-		// $this->form_validation->set_rules('postcode', 'postcode', 'required');
-		// $this->form_validation->set_rules('tel', 'Telephone', 'required');
+		$this->form_validation->set_rules('salutation', 'salutation', 'required');
+		$this->form_validation->set_rules('first_name', 'first_name', 'required');
+		$this->form_validation->set_rules('last_name', 'last_name', 'required');
+		$this->form_validation->set_rules('DOB', 'Date Of Birth', 'required');
+		$this->form_validation->set_rules('address', 'Address', 'required');
+		$this->form_validation->set_rules('city', 'city', 'required');
+		$this->form_validation->set_rules('postcode', 'postcode', 'required');
+		$this->form_validation->set_rules('tel', 'Telephone', 'required');
 		
 		$data['title'] = 'Create contact:';
 
 		if ($this->form_validation->run() === FALSE){
-			
+			 
 			$this->load->view('templates/header', $data);
 			$this->load->view('contactsdb/index');
 			$this->load->view('contactsdb/createcontact', $data);
@@ -80,95 +118,35 @@ class Contact_controller extends CI_Controller {
 		{					
 			$slug=$this->input->post();
 			$data['contact'] = $this->contact_model->set_contact($slug);
-			$data['title'] = 'Contact created successfully!';
-			$this->load->view('templates/header', $data);
-			//On success go back to main index.
-			$this->load->view('contactsdb/index');
-			$this->load->view('templates/footer');
+			if($data['contact']>0){
+				
+				//using success with updated message 
+				$this->load->view('contactsdb/index');
+				$this->load->view('contactsdb/success');
+				$this->load->view('templates/footer');
+			}
+			else{
+		
+				//using fail from updated .
+				$this->load->view('contactsdb/index');
+				$this->load->view('contactsdb/fail');
+				$this->load->view('templates/footer');
+			}
 		
 		}
 
-	}
-
-		
-	public function showcontact($slug = NULL){
-
-		$data['title'] = "Show Contact";
-		$this->load->helper('form');
-		$this->load->library('form_validation');
-		//x is the selected choice from index.php
-		
-		$slug=$this->input->post();
-		// echo '>>>>>>posted>>>>>'.var_dump($slug);
-		// exit;
-		$data['contact'] = $this->contact_model->get_contact($slug);
-		
-		
-		
-		if (empty($data['contact'])){
-				show_404();
-		}
-
-		$this->load->view('templates/header', $data);
-		$this->load->view('contactsdb/index');
-		$this->load->view('contactsdb/showcontact', $data);
-		$this->load->view('templates/footer');
 	}
 	
-	public function editcontact($slug = NULL){	
-		$slug=$this->input->post();
-		// echo '>>>>>>show>>>>>'.var_dump($slug);
-		// exit;
-		$this->load->helper('form');
-		$this->load->library('form_validation');
-		$data['contact'] = $this->contact_model->get_contact($slug);
-		$data['title'] = 'Edit:';
-
+	public function index(){
+		
+		$data['title'] = 'Contacts Database:';
 		$this->load->view('templates/header', $data);
 		$this->load->view('contactsdb/index');
-		$this->load->view('contactsdb/editcontact', $data);
 		$this->load->view('templates/footer');
 	}
-	public function updatecontact($slug = NULL){
-		
-		$data['title'] = 'Update a contact';
-		$slug=$this->input->post();
-		$data['contact'] = $this->contact_model-> update_contact($slug);
 
-		//echo ">>>update-model.id..>>>>".var_dump($data['contact']);
-		//exit;
-		if($data['contact']>0){
-			
-			$data['title'] = 'Update Successfully';
-			$this->load->view('templates/header', $data);
-			$this->load->view('contactsdb/index');
-			$this->load->view('contactsdb/success', $data);
-			$this->load->view('templates/footer');
-	
-		}
-		else{
-			
-			//show_404();
-			$data['title'] = 'Update Unsuccessfully';
-			$this->load->view('templates/header', $data);
-			$this->load->view('contactsdb/index');
-			$this->load->view('contactsdb/fail', $data);
-			$this->load->view('templates/footer');
-		}
-	}
  
-		public function searchcontact(){
-		
-		$this->load->helper('form');
-		$this->load->library('form_validation');
-		//$data['contact'] = $this->contact_model->get_contact($slug);
-		$data['title'] = 'Search:';
-
-		$this->load->view('templates/header', $data);
-		$this->load->view('contactsdb/index');
-		$this->load->view('contactsdb/searchcontact', $data);
-		$this->load->view('templates/footer');
-	}
+	
 
 		
 }
