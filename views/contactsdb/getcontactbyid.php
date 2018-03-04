37d72b7c9d43eb79ed1390682579d40f8094ef7b
steelslugs <34694950+steelslugs@users.noreply.github.com>
2018-03-04 17:03:02 +0000

    Update getcontactbyid.php

--- views/contactsdb/getcontactbyid.php
+++ views/contactsdb/getcontactbyid.php
@@ -1,7 +1,7 @@
 <h2><?php echo $title; ?></h2>
 <?php echo validation_errors(); ?>
 
-<?php echo form_open('Contact_controller/showcontact'); ?>
+<?php echo form_open('Contact_controller/viewit/showcontact'); ?>
 
     <label for="contact_id">contact_id</label>
     <input type="input" name="contact_id" /><br />     
